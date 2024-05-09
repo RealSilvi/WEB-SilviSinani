d9421151a8b369e0f46d1be2e9b22f44c9d703d8
RealSilvi <silvi.sinani@studio.unibo.it>
2024-05-09 20:08:09 +0200

    seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,21 +2,31 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Profile;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory()->create([
+            'first_name' => 'admin',
+            'last_name' => 'admin',
+            'email' => 'admin@example.com',
+            'password' => 'password'
+        ]);
 
-        // \App\Models\User::factory()->create([
-        //     'firs_name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $users = User::factory(50)->create();
+
+        foreach ($users as $user) {
+            $howManyProfiles = rand(0, 4);
+            for ($i = 0; $i <= $howManyProfiles; $i++) {
+
+                Profile::factory()->for($user)->realImages()->create([
+                    'default' => $i===0,
+                ]);
+            }
+        }
     }
 }
