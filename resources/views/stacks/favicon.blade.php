f4fb20b3cfb8f3cae22c847b66e4360bb2075438
RealSilvi <silvi.sinani@studio.unibo.it>
2024-06-03 20:21:10 +0200

    favico

--- resources/views/stacks/favicon.blade.php
+++ resources/views/stacks/favicon.blade.php
@@ -1,10 +1,12 @@
 @push('head')
-    {{--    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png?v=1"> --}}
-    {{--    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png?v=1"> --}}
-    {{--    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png?v=1"> --}}
-    {{--    <link rel="manifest" href="/site.webmanifest?v=1"> --}}
-    {{--    <link rel="mask-icon" href="/safari-pinned-tab.svg?v=1" color="#000000"> --}}
-    {{--    <link rel="shortcut icon" href="/favicon.ico?v=1"> --}}
-    {{--    <meta name="msapplication-TileColor" content="#ffffff"> --}}
-    {{--    <meta name="theme-color" content="#ffffff"> --}}
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png?v=1">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png?v=1">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png?v=1">
+    <link rel="manifest" href="/site.webmanifest?v=1">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg?v=1" color="#000000">
+    <link rel="shortcut icon" href="/favicon.ico?v=1">
+    <meta name="apple-mobile-web-app-title" content="web24-silvisinani">
+    <meta name="application-name" content="web24-silvisinani">
+    <meta name="msapplication-TileColor" content="#00a300">
+    <meta name="theme-color" content="#ffffff">
 @endpush
